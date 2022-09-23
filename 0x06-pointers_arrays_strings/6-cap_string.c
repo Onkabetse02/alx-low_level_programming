b9a9a88866696c3e36fa46d98bd17801362e441b
Onkabetse Mogomotsi <mogomotsionkabetse@gmail.com>
2022-09-23 03:37:03 -0700

    Cap strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,37 @@
 #include "main.h"
+
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase
- * @s: input string.
- * Return: the pointer to dest.
+ * cap_string - capitalizes all words of a string.
+ * @str: string to be modified
+ * Return: the resulting string
  */
-
-char *string_toupper(char *s)
+char *cap_string(char *str)
 {
-  int count = 0;
+  int index = 0;
 
-  while (*(s + count) != '\0')
+  while (str[index])
     {
-      if ((*(s + count) >= 97) && (*(s + count) <= 122))
-	*(s + count) = *(s + count) - 32;
-      count++;
+      while (!(str[index] >= 'a' && str[index] <= 'z'))
+	index++;
+
+      if (str[index - 1] == ' ' ||
+	  str[index - 1] == '\t' ||
+	  str[index - 1] == '\n' ||
+	  str[index - 1] == ',' ||
+	  str[index - 1] == ';' ||
+	  str[index - 1] == '.' ||
+	  str[index - 1] == '!' ||
+	  str[index - 1] == '?' ||
+	  str[index - 1] == '"' ||
+	  str[index - 1] == '(' ||
+	  str[index - 1] == ')' ||
+	  str[index - 1] == '{' ||
+	  str[index - 1] == '}' ||
+	  index == 0)
+	str[index] -= 32;
+
+      index++;
     }
 
-  return (s);
+  return (str);
 }
