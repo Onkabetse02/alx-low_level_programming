14f5cbe7aafb79bcad4855cae53a9a205bd5c588
Onkabetse02 <mogomotsionkabetse@gmail.com>
2023-10-26 10:57:29 -0700

    100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,9 +7,9 @@
  * @index: index
  * Return: 1 if success, -1 if error
  */
-int clear_bit(unsigned long int *n, unsigned index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(n) *8)
+	if (index > sizeof(n) * 8)
 		return (-1);
 
 	*n &= ~(1 << index);
